46c62f7f0fe638a106668eb41905f714284f47f6
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-03-07 22:04:26 +0100

    Commence server

--- whbcs.py
+++ whbcs.py
@@ -5,3 +5,98 @@
 
 APPNAME = 'WHBCS'
 VERSION = '2.0-pre'
+
+import sys, os, socket
+import signal
+import logging
+
+HOST = ''
+PORT = 4321
+
+class ClientHandler:
+    def __init__(self, server, id, sock, addr, logger=None):
+        self.server = server
+        self.id = id
+        self.socket = sock
+        self.addr = addr
+        self.logger = logger
+
+class Server:
+    @classmethod
+    def listen(cls, addr, logger=None):
+        if logger:
+            logger.info('LISTENING bind=%s:%s' % addr)
+        s = socket.socket()
+        s.bind(addr)
+        s.listen(5)
+        return cls(s, logger)
+
+    def __init__(self, socket, logger=None):
+        self.socket = socket
+        self.logger = logger
+        self._next_connid = 0
+
+    def close(self):
+        self.log('CLOSING')
+        self.socket.close()
+
+    def log(self, *args):
+        if self.logger: self.logger.info(*args)
+
+    def __call__(self):
+        while 1:
+            conn, addr = self.socket.accept()
+            cid = self._next_connid
+            self._next_connid += 1
+            self.log('CONNECTION id=%r from=%r' % (cid, addr))
+            spawn_thread(ClientHandler(self, cid, conn, addr, logger))
+            conn, addr = None, None
+
+def main():
+    # Interrupt execution
+    def die(msg, retcode=1):
+        sys.stderr.write(msg)
+        sys.stderr.flush()
+        sys.exit(retcode)
+    # Signal handler
+    def interrupt(signo, frame):
+        raise KeyboardInterrupt
+    # Parse arguments
+    host, port, logfile = HOST, PORT, None
+    try:
+        it = iter(sys.argv)
+        for arg in it:
+            if arg == '--help':
+                die('USAGE: %s [--help] [--host host] [--port port] '
+                    '[--logfile logfile]\n' % sys.argv[0], 0)
+            elif arg == '--host':
+                host = next(it)
+            elif arg == '--port':
+                port = int(next(it))
+            elif arg == '--logfile':
+                logfile = next(it)
+            else:
+                die('ERROR: Unrecognized argument %r\n' % arg)
+    except StopIteration:
+        die('ERROR: Missing required argument for option %r\n' % arg)
+    except ValueError:
+        die('ERROR: Incorrect argument for option %r\n' % arg)
+    # Set up logging
+    config = {}
+    if logfile is not None: config['filename'] = logfile
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s %(name)s '
+        '%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S', **config)
+    # Trap signals
+    signal.signal(signal.SIGINT, interrupt)
+    signal.signal(signal.SIGTERM, interrupt)
+    # Run server
+    logging.info(APPNAME + ' ' + VERSION)
+    s = Server.listen((host, port), logging.getLogger())
+    try:
+        s.main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        s.close()
+
+if __name__ == '__main__': main()
